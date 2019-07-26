9d4c5123a249c06e0a5730e60e5753634b541591
zhangzuxiong <762637517@qq.com>
2019-07-26 10:53:00 +0800

    数组实现栈

---
+++ project/ConsoleApplication25/ConsoleApplication25.cpp
@@ -0,0 +1,63 @@
+﻿// ConsoleApplication25.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//数组实现栈
+
+#include "Stack_By_Array.h"
+
+void testArrayList() {
+	ArrayList array = { 0 };
+
+	initArrayList(&array);
+
+	printArrayList(array);
+
+	insertArrayList(&array, 1, 1);
+
+
+	insertArrayList(&array, 1, 2);
+
+
+	insertArrayList(&array, 1, 3);
+
+	insertArrayList(&array, 1, 4);
+
+
+	insertArrayList(&array, 1, 5);
+
+	printArrayList(array);
+
+	printf("\n%d\n", getArrayList(&array, 0));
+	printArrayList(array);
+
+	clearArrayList(&array);
+
+
+	printArrayList(array);
+}
+
+void testStackByArrayList() {
+	Stack stack = { 0 };
+	initStack(&stack);
+	printStack(stack);
+	for (size_t i = 0; i < 10; i++)
+	{
+		pushStack(&stack, (i + 1) * 10);
+	}
+
+	printStack(stack);
+
+	printf("\n出栈元素为:%d\n", popStack(&stack));
+	printf("\n出栈元素为:%d\n", popStack(&stack));
+
+	printStack(stack);
+
+	clearStack(&stack);
+	printStack(stack);
+
+}
+
+int main()
+{
+	testStackByArrayList();
+
+    return 0;
+}
---
+++ project/ConsoleApplication25/Stack_By_Array.h
@@ -0,0 +1,44 @@
+#pragma once
+
+
+#include "Array_List.h"
+
+typedef struct Stack
+{
+	//数组
+	ArrayList list;
+
+	//栈的最大容量
+	int max;
+
+	//栈的有效元素个数
+	int count;
+
+} Stack;
+
+
+// 1.初始化
+void initStack(Stack* p);
+
+
+// 2.判空 如果为空，则为真；否则为假
+int isEmpty(const Stack stack);
+
+
+// 3.判满 如果为满，则为真；否则为假
+int isFull(const Stack stack);
+
+
+// 4.压入,p 表示需要操作的栈是谁， num 表示需要压入栈的数值是多少
+void pushStack(Stack* p, const int num);
+
+
+// 5.弹出
+int popStack(Stack* p);
+
+
+// 6.清空
+void clearStack(Stack* p);
+
+// 7.打印
+void printStack(const Stack stack);
